159e44c262da910a5f344e51ab613959ae482da5
Pasan Dissanayake <pasandissanayake@gmail.com>
2020-05-10 19:53:41 +0530

    massive reorganization

--- ros-packages/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pasan/kalani/ros-packages/src
-# Build directory: /home/pasan/kalani/ros-packages/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(filter_v1)
--- ros-packages/build/filter_v1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pasan/kalani/ros-packages/src/filter_v1
-# Build directory: /home/pasan/kalani/ros-packages/build/filter_v1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
