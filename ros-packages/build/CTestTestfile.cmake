254e65513ff31f90d94ded9553993b6afbc1c4be
Pasan Dissanayake <pasandissanayake@gmail.com>
2020-05-10 12:56:53 +0530

    begining ros implementation

---
+++ ros-packages/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/pasan/kalani/ros-packages/src
+# Build directory: /home/pasan/kalani/ros-packages/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(filter_v1)
